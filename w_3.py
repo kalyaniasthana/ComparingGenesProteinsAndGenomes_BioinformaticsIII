3048a070b4da31142dab45e3bd34aa4f302cb11c
Kalyani Asthana <kalyaniasthana@gmail.com>
2019-01-13 21:43:09 +0530

    Middle Edge Problem

--- w_3.py
+++ w_3.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+from __future__ import print_function, division
 import copy
 
 pam250 = {'A': {'A': 2, 'C': -2, 'E': 0, 'D': 0, 'G': 1, 'F': -3, 'I': -1, 'H': -1, 'K': -1, 'M': -1, 'L': -2, 'N': 0, 'Q': 0, 'P': 1, 'S': 1, 'R': -2, 'T': 1, 'W': -6, 'V': 0, 'Y': -3}, 'C': {'A': -2, 'C': 12, 'E': -5, 'D': -5, 'G': -3, 'F': -4, 'I': -2, 'H': -3, 'K': -5, 'M': -5, 'L': -6, 'N': -4, 'Q': -5, 'P': -3, 'S': 0, 'R': -4, 'T': -2, 'W': -8, 'V': -2, 'Y': 0}, 'E': {'A': 0, 'C': -5, 'E': 4, 'D': 3, 'G': 0, 'F': -5, 'I': -2, 'H': 1, 'K': 0, 'M': -2, 'L': -3, 'N': 1, 'Q': 2, 'P': -1, 'S': 0, 'R': -1, 'T': 0, 'W': -7, 'V': -2, 'Y': -4}, 'D': {'A': 0, 'C': -5, 'E': 3, 'D': 4, 'G': 1, 'F': -6, 'I': -2, 'H': 1, 'K': 0, 'M': -3, 'L': -4, 'N': 2, 'Q': 2, 'P': -1, 'S': 0, 'R': -1, 'T': 0, 'W': -7, 'V': -2, 'Y': -4}, 'G': {'A': 1, 'C': -3, 'E': 0, 'D': 1, 'G': 5, 'F': -5, 'I': -3, 'H': -2, 'K': -2, 'M': -3, 'L': -4, 'N': 0, 'Q': -1, 'P': 0, 'S': 1, 'R': -3, 'T': 0, 'W': -7, 'V': -1, 'Y': -5}, 'F': {'A': -3, 'C': -4, 'E': -5, 'D': -6, 'G': -5, 'F': 9, 'I': 1, 'H': -2, 'K': -5, 'M': 0, 'L': 2, 'N': -3, 'Q': -5, 'P': -5, 'S': -3, 'R': -4, 'T': -3, 'W': 0, 'V': -1, 'Y': 7}, 'I': {'A': -1, 'C': -2, 'E': -2, 'D': -2, 'G': -3, 'F': 1, 'I': 5, 'H': -2, 'K': -2, 'M': 2, 'L': 2, 'N': -2, 'Q': -2, 'P': -2, 'S': -1, 'R': -2, 'T': 0, 'W': -5, 'V': 4, 'Y': -1}, 'H': {'A': -1, 'C': -3, 'E': 1, 'D': 1, 'G': -2, 'F': -2, 'I': -2, 'H': 6, 'K': 0, 'M': -2, 'L': -2, 'N': 2, 'Q': 3, 'P': 0, 'S': -1, 'R': 2, 'T': -1, 'W': -3, 'V': -2, 'Y': 0}, 'K': {'A': -1, 'C': -5, 'E': 0, 'D': 0, 'G': -2, 'F': -5, 'I': -2, 'H': 0, 'K': 5, 'M': 0, 'L': -3, 'N': 1, 'Q': 1, 'P': -1, 'S': 0, 'R': 3, 'T': 0, 'W': -3, 'V': -2, 'Y': -4}, 'M': {'A': -1, 'C': -5, 'E': -2, 'D': -3, 'G': -3, 'F': 0, 'I': 2, 'H': -2, 'K': 0, 'M': 6, 'L': 4, 'N': -2, 'Q': -1, 'P': -2, 'S': -2, 'R': 0, 'T': -1, 'W': -4, 'V': 2, 'Y': -2}, 'L': {'A': -2, 'C': -6, 'E': -3, 'D': -4, 'G': -4, 'F': 2, 'I': 2, 'H': -2, 'K': -3, 'M': 4, 'L': 6, 'N': -3, 'Q': -2, 'P': -3, 'S': -3, 'R': -3, 'T': -2, 'W': -2, 'V': 2, 'Y': -1}, 'N': {'A': 0, 'C': -4, 'E': 1, 'D': 2, 'G': 0, 'F': -3, 'I': -2, 'H': 2, 'K': 1, 'M': -2, 'L': -3, 'N': 2, 'Q': 1, 'P': 0, 'S': 1, 'R': 0, 'T': 0, 'W': -4, 'V': -2, 'Y': -2}, 'Q': {'A': 0, 'C': -5, 'E': 2, 'D': 2, 'G': -1, 'F': -5, 'I': -2, 'H': 3, 'K': 1, 'M': -1, 'L': -2, 'N': 1, 'Q': 4, 'P': 0, 'S': -1, 'R': 1, 'T': -1, 'W': -5, 'V': -2, 'Y': -4}, 'P': {'A': 1, 'C': -3, 'E': -1, 'D': -1, 'G': 0, 'F': -5, 'I': -2, 'H': 0, 'K': -1, 'M': -2, 'L': -3, 'N': 0, 'Q': 0, 'P': 6, 'S': 1, 'R': 0, 'T': 0, 'W': -6, 'V': -1, 'Y': -5}, 'S': {'A': 1, 'C': 0, 'E': 0, 'D': 0, 'G': 1, 'F': -3, 'I': -1, 'H': -1, 'K': 0, 'M': -2, 'L': -3, 'N': 1, 'Q': -1, 'P': 1, 'S': 2, 'R': 0, 'T': 1, 'W': -2, 'V': -1, 'Y': -3}, 'R': {'A': -2, 'C': -4, 'E': -1, 'D': -1, 'G': -3, 'F': -4, 'I': -2, 'H': 2, 'K': 3, 'M': 0, 'L': -3, 'N': 0, 'Q': 1, 'P': 0, 'S': 0, 'R': 6, 'T': -1, 'W': 2, 'V': -2, 'Y': -4}, 'T': {'A': 1, 'C': -2, 'E': 0, 'D': 0, 'G': 0, 'F': -3, 'I': 0, 'H': -1, 'K': 0, 'M': -1, 'L': -2, 'N': 0, 'Q': -1, 'P': 0, 'S': 1, 'R': -1, 'T': 3, 'W': -5, 'V': 0, 'Y': -3}, 'W': {'A': -6, 'C': -8, 'E': -7, 'D': -7, 'G': -7, 'F': 0, 'I': -5, 'H': -3, 'K': -3, 'M': -4, 'L': -2, 'N': -4, 'Q': -5, 'P': -6, 'S': -2, 'R': 2, 'T': -5, 'W': 17, 'V': -6, 'Y': 0}, 'V': {'A': 0, 'C': -2, 'E': -2, 'D': -2, 'G': -1, 'F': -1, 'I': 4, 'H': -2, 'K': -2, 'M': 2, 'L': 2, 'N': -2, 'Q': -2, 'P': -1, 'S': -1, 'R': -2, 'T': 0, 'W': -6, 'V': 4, 'Y': -2}, 'Y': {'A': -3, 'C': 0, 'E': -4, 'D': -4, 'G': -5, 'F': 7, 'I': -1, 'H': 0, 'K': -4, 'M': -2, 'L': -1, 'N': -2, 'Q': -4, 'P': -5, 'S': -3, 'R': -4, 'T': -3, 'W': 0, 'V': -2, 'Y': 10}}
@@ -6,6 +6,7 @@ blosum62 = {'A': {'A': 4, 'C': 0, 'E': -1, 'D': -2, 'G': 0, 'F': -2, 'I': -1, 'H
 
 sigma = -11
 epsilon = -1
+indel_penalty = -5
 
 def AffineGapPenalties(v, w):
 	lower = []
@@ -98,11 +99,87 @@ def AffineGapPenalties(v, w):
 
 	return score, k , l
 
+def MiddlePath(v, w):
+	s = []
+	p = len(v)
+	q = len(w)
+	s = [[0 for x in xrange(q+1)] for x in xrange(p+1)] 
 
-v = 'RRCIKGPDLQFMMREEAEWDENNMQVWTWALEIIFNVACSEIHLSQSWMFGMPFVLDYATDEMQHHQCEVAECPHFQFRR'
-w = 'RRCIKGHVDEQRYLQFMMREEAEHDMQVMWLETWALLGCEIIRNGEMHGACSVIVLSQSWMFGMYATDEMQHHQCEVAEVPHFYIDCRR'
+	for i in range(1, p+1):
+		s[i][0] = i*indel_penalty
+	for j in range(1, q+1):
+		s[0][j] = j*indel_penalty
+
+	for i in range(1, p+1):
+	    for j in range(1, q+1):
+	    	s[i][j] = max(s[i-1][j-1] + blosum62[v[i-1]][w[j-1]], s[i-1][j] + indel_penalty, s[i][j-1] + indel_penalty)
+
+	i = p
+	j = q
+	#print(s)
+	if j%2 == 0:
+		res = []
+		m1 = i//2
+		m2 = i//2 -1 
+		#print(m1, m2)
+		while i > 0 and j > 0:
+			if s[i][j] == s[i-1][j-1] + blosum62[v[i-1]][w[j-1]]: 
+				if j == m1 and (j - 1) == m2:
+					res.append((i-1, m2))
+					res.append((i, m1))
+					break
+				i -= 1
+				j -= 1
+			elif s[i][j] == s[i-1][j] + indel_penalty:
+				if j == m1:
+					res.append((i, m1))
+					res.append((i-1, m1))
+				break
+				i -= 1
+			elif s[i][j] == s[i][j-1] + indel_penalty:
+				if j == m1:
+					res.append((i, m1))
+					res.append((i, m2))
+				break
+				j -= 1
+		return res
+	else:
+		res = []
+		m = j//2
+		#print(m)
+		while i > 0 and j > 0:
+			if s[i][j] == s[i-1][j-1] + blosum62[v[i-1]][w[j-1]]: 
+				if j == m+1:
+					res.append((i-1, j-1))
+					res.append((i, j))
+					break
+				i -= 1
+				j -= 1
+			elif s[i][j] == s[i-1][j] + indel_penalty:
+				if j == m:
+					res.append((i-1, j))
+					res.append((i, j))
+					break
+				i -= 1
+			elif s[i][j] == s[i][j-1] + indel_penalty:
+				if j == m+1:
+					res.append((i, j-1))
+					res.append((i, j))
+					break
+				j -= 1
+		return res 
+
+
+v = 'PAACYFVATLREEGKKHTQITTSMEKKHIGMPVTMWRVSQPYVYHFVQDELCIKTAREKHAYVCHSGKLRDLWYPLFKMSKTDEVFRLMGVWIIACGRCVTVHRLCMCTLHRTKFPLFCCTDGLTHRLRYYYYYINPAFMTQVYTHAMWWGDKLSATYLAYIMKSHFALSRGNDQNGTENIPSATCQSCRMHSAPALHNTCHNMKRRTCYMGHHQREYRGRYRDPVDSVFHVRIRRNGQKNCCGWQWYTHYKCHQWDTPPDVYVKLNVQKDTGLNIYRSNAVKTLTGPAVQYVTGVFFWPEHQVTSETDCQKCIEPAARAGFGRPTLCHTEPIGYKLGVSVPFQTSWMRSRCLWNVNLMRVKYGGHEVSDSIEPRASQMVHHFPGGSFQCPDVDFNPNHWSTTQIKFGVTGQTARWDRGSAFRYRWAYLVFGWMQDKSVNQKQDCGRYKAAKCPKCCMYSRFFVMQHRDHWTCNNPIYHPLATPDWPKPMKLWQWHYLGLVELYPPMGLHVCKNRTRPGWTMSMFLMKGHCPNHYPMVSNCDKMPTLDQKQKWMNCVWWKFGSWGNCGDICGTQYACPLCWIWMHHRKDCAFKFEAQASSPSSCRDHGHFWMLVNLDDVILHEKAKVTCTCRPFPFRPMSYSEVIPQCADMDPCETCLAKWVSLHDKFVTNCFKWYWVAYKMHLCCLRPPESEGSTTHPPVGCDSGDELKGEKEQQQWIDVSVGMDMQPIMPYFETRQVHIWSRTPFNQIIAHYHHRISSMFSTRRAGFCGIAPHYMPHTDWWLANDNMKGNMNKKEDMSFDNCDKYGWFPRDFAPKMHMPHTDQRKAEYQCIYQYMFARSHMNCDFKMMDFASNFGCNVCESIAWMSDIVKDQYRLKKAACNDSHWDNMHDASSTNCQFGYSAAIGEDYMYFIPMPFMFDSNLPDNWPAWKGADVLKAPCGCITDNIMMPIHWNSKLMFDFNEIRWIGIIKNFETSHGVHWDGNFMDVKAHDKCFPKDCHGHVAMAQYFVSCCKEDKFMIMCPQRRLGWQPQEA'
+w = 'SQGSVGLCEFQIWAIAWCFHFAMRQMQHSIVMYYDCADLITHMTNTIEECTWHVYHYPMIRNYFDSPPYQLMKEMTCHWWFTHHIDNHLSKLLEDVAWKLAGRFPSQIVVQYLVAYYERQLCGCRQLFDWDRCSMIVRFVNMHPHHFYPPYWRTCLETKTCSHITDVDHFQHDHCPNNKAPEGWHHEYGGMVDLKFKINMRSIGRCGFFYAMEWTTCLVQIFTWNLPIFTSHCIWKKLMALEMPENIGKALFADVCMYQDWKLAMVSSQIRGGGATPPIWFWIICPFRTFGQCEWGGKNSTPEDFNMIFDPVWKAIDYVAASYFCYDFVACPIKNHRSVPMIPWGAYVLAHHLKRLKDYKGCFNVHTHIGPVKPFKNKRDHECPCWVYKQLTIDMQSNTEAYEMTWCRPSNECNCVLYMNSDVRCAIRCGHQTGVVCIDWKALGQQVDEIAKCPWCCMYSRHFVYQPLATPDWPFPMKLWQWHYLGLVELYPDGLHVCKNRTRPGWTMSMFLMKGHCPNAYPCVSNCDKMMTLDYKWWKFGSTGNCGDICGTQYACPLCWMWMQYEHAAICLSESLNMYSVINQFMKWWFNMYCYWNEPAKEPNLLHGGVWNGFACDWHNCMWRNVLFPNWEEFPTDKMQFVDSFATQGLYVNTNAKFCHTRPYNLDMQHIIGIVANVHHFRSSLAKEKYTTQIQMHLICAEDVIWSHCYHFHLTYVRNLQLSFAMRIYDSKRVIALVCNSNGIDYWMKMSDFVESNFSGWKSFFAIEKLPRGDSMHRHVVPEIRPTHCKYPHKPMIPRYHYASIMMLYPHLTVCKLHVDAQQWMLVGNHLMMRHYAQFFNFTDGEEFNSFRVWNLHDTDDGFQEDGINDILWLRMGPMFWYLPIDKYMKYTEYTKIKHWTWYAQCESMMFDHHNESHLWPFEKGSRHNIKKRYENMLAAKEKMMRMNVWEMNTKRWHQEQRHAGAYDIYQSYRHFIEPSHEDKEHRDCPYRQASEGLHIFTSFGDFLFFVLAEK'
+'''
 score, k, l = AffineGapPenalties(v, w)
 print(score)
 print(k)
 print(l)
+'''
+res = MiddlePath(v, w)
+res = [str(i) for i in res]
+res = ' '.join(res)
+print(res)
 
